508f3775f031e2a464fbd3ace1365cabc3d7bc3b
pawinhon1 <33918211+pawinhon1@users.noreply.github.com>
2020-08-18 11:50:32 +0700

    Add files via upload
    insert-view-update-delete

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 require('db.php');
 include("auth.php");
 ?>
@@ -7,14 +7,15 @@ include("auth.php");
 <head>
 <meta charset="utf-8">
 <title>Dashboard - Secured Page</title>
-<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="style.css" />
 </head>
 <body>
 <div class="form">
-<p>Dashboard</p>
-<p>This is another secured page.</p>
-<p><a href="index.php">Home</a></p>
-<a href="logout.php">Logout</a>
+<p>Welcome to Dashboard.</p>
+<p><a href="index.php">Home</a><p>
+<p><a href="insert.php">Insert New Record</a></p>
+<p><a href="view.php">View Records</a><p>
+<p><a href="logout.php">Logout</a></p>
 </div>
 </body>
 </html>
